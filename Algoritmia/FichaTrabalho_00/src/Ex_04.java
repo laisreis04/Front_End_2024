02cda345b7638da5759b3d1243a14009983ac702
Lais Reis <laisreis04@gmail.com>
2024-11-06 10:32:27 +0000

    Correção exercício 4

---
+++ Algoritmia/FichaTrabalho_00/src/Ex_04.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex_04 {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        //Variaveis
+        double raio, area, pi = 3.14;
+
+        //Pedir raio
+        System.out.println("Inisira o raio em cm: ");
+        raio = input.nextDouble();
+
+        //Calcular área
+        area = pi * (raio*raio);
+
+        //Imprimir
+
+        System.out.println("Área = " + area + " cm");
+    }
+}
