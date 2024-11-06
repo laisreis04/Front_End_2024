69fb9395c8b784f5a289f86a41bdfe393e6d0003
Lais Reis <laisreis04@gmail.com>
2024-11-06 10:04:08 +0000

    Correção dos exercícios 2 e 3

--- Algoritmia/FichaTrabalho_00/src/Ex_02.java
+++ Algoritmia/FichaTrabalho_00/src/Ex_02.java
@@ -1,2 +1,36 @@
+import java.util.Scanner;
+
 public class Ex_02 {
+
+    public static void main(String[] args){
+
+        //Pedir ao user 2 numeros
+        Scanner input = new Scanner(System.in);
+
+        int num1, num2,soma,subtracao,multiplicacao, divisao;
+
+        System.out.print("Insira um número: ");
+        num1 = input.nextInt();
+        System.out.print("Insira um número: ");
+        num2 = input.nextInt();
+
+        //Calcular soma
+       soma = num1 + num2;
+
+        //Calcular subtração
+        subtracao = num1 - num2;
+
+        //Calcular divisão
+        divisao = num1 / num2;
+
+        //Calcular multiplicação
+        multiplicacao = num1 * num2;
+
+        //Imprimir o resultado na consola
+
+        System.out.println("Soma = " + soma);
+        System.out.println("Subtração = " + subtracao);
+        System.out.println("Divisão = " + divisao);
+        System.out.println("Multiplicação = " + multiplicacao);
+    }
 }
---
+++ Algoritmia/FichaTrabalho_00/src/Ex_03.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ex_03 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        //Insira largura
+        System.out.println("Insira a largura: ");
+        double largura = input.nextDouble();
+
+        //Insira altura
+        System.out.println("Insira altura: ");
+        double altura = input.nextDouble();
+
+        //Calcular área
+
+        double area = largura * altura;
+
+        //Calcular Perimetro
+        double perimetro = 2 * (largura) + 2 * (altura);
+
+        //Imprimir o resultado
+        System.out.println("Área = " + area);
+        System.out.println("Perimetro = " + perimetro);
+
+    }
+}
