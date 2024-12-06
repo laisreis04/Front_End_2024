fa5ac4e879f77b025bbd5d2b6b64d8a7c7f5b9d1
Lais <laisreis04@gmail.com>
2024-12-06 12:35:35 +0000

    Ficha

--- Algoritmia/FichaTrabalho_00/src/FichaPratica_04/Ex_13.java
+++ Algoritmia/FichaTrabalho_00/src/FichaPratica_04/Ex_13.java
@@ -1,2 +1,34 @@
-package FichaPratica_04;public class Ex_13 {
+package FichaPratica_04;
+
+import java.util.Scanner;
+
+public class Ex_13 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[][] matriz = new int[4][4];
+        int somaDiagonal = 0;
+
+        //Preencher a Matriz
+        for(int linha = 0; linha < matriz.length; linha++){
+            for (int coluna = 0; coluna < matriz.length; coluna++){
+                System.out.print("Insira os valores: ");
+                matriz[linha][coluna] = input.nextInt();
+            }
+        }
+        //Somar os valores da diagnal
+        for(int linha = 0; linha < matriz.length; linha++){
+            for(int coluna = 0; coluna < matriz.length; coluna++){
+                if(linha ===)
+            }
+        }
+
+        //Imprimir a matriz
+
+        //Resultado da soma
+
+
+    }
 }
