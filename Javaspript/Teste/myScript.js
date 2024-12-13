82bb2693eddd19f32b8d6e07d374a495254473a4
Lais Reis <laisreis04@gmail.com>
2024-12-13 16:48:34 +0000

    Fichas

---
+++ Javaspript/Teste/myScript.js
@@ -0,0 +1,13 @@
+
+//Para aparecer na consola
+console.log('Vou para a consola')
+
+//Para criar um pop-up
+//alert('Hello! vou para o Browser')
+
+//Criar um pormpt no borwser
+let myName = 'Qual o seu nome?'
+
+//Para aparecer no browser
+console.log(myName)
+console.log('Hello')
