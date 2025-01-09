566f7a7eabd706de0f63a679a69124547853553a
Lais <laisreis04@gmail.com>
2025-01-09 21:27:34 +0000

    update

---
+++ Javascript/FichaPratica_03/ex_01.js
@@ -0,0 +1,11 @@
+function ehPalindromo(palavra) {
+    const palavraInvertida = palavra.split('').reverse().join('');
+    if(palavra === palavraInvertida){
+        return true
+    }else{
+        return false
+    }
+    
+  }
+
+  console.log(ehPalindromo('arara'))
