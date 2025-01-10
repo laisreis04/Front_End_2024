957e3087dc29de2157920155d5e124320f3b706d
Lais Reis <laisreis04@gmail.com>
2025-01-10 14:50:08 +0000

    update

--- Javascript/DOM/script.js
+++ Javascript/DOM/script.js
@@ -9,3 +9,28 @@
 // Pickles
 // let name = document.querySelector('h1')
 // name.innerText = 'Que horror'
+
+// function allEvens(array){
+//     return array.every(pares => pares %2 === 0)
+// }
+
+// console.log(allEvens([3,7,5,3,1])) //false
+// console.log(allEvens([2,4,6,8])) //true
+// console.log(allEvens([1,12,7,4,8])) //false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
