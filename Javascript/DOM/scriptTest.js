45c2ae970beddb8f71d747f04ddab919ccf651e2
Lais Reis <laisreis04@gmail.com>
2025-01-14 16:03:08 +0000

    update

---
+++ Javascript/DOM/scriptTest.js
@@ -0,0 +1,7 @@
+// const spanElements = document.querySelectorAll('span')
+
+// const cores =  ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// spanElements.forEach((mudarSpan , index) => {
+//     mudarSpan.style.backgroundColor = cores[index]
+// });
