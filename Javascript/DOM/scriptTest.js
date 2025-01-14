706a88f2b9d96e18fd1871aad4165ca6db79027a
Lais Reis <laisreis04@gmail.com>
2025-01-14 16:50:36 +0000

    update

--- Javascript/DOM/scriptTest.js
+++ Javascript/DOM/scriptTest.js
@@ -6,3 +6,18 @@
 //     mudarSpan.style.backgroundColor = cores[index]
     
 // });
+
+function mudarCoresHighlight(){
+
+const elementosH1 = document.querySelectorAll('.highlight');
+const novoHigh = document.querySelectorAll('.semHigh')
+
+elementosH1.forEach(cor => {
+    elementosH1.classList.remove('yellow')
+})
+
+novoHigh.forEach(novaCor => {
+    novoHigh.classList.add('yellow')
+})
+
+}
