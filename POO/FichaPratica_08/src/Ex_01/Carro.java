c7fb93206073f99a19cd4296c06989a8de20dd4b
Lais Reis <laisreis04@gmail.com>
2025-01-20 17:00:26 +0000

    Update

--- POO/FichaPratica_08/src/Ex_01/Carro.java
+++ POO/FichaPratica_08/src/Ex_01/Carro.java
@@ -12,6 +12,7 @@ public class Carro {
     Tipo_Combustivel tipoCombustivel;
     private double consumo100;
 
+    //Construtor
 
     public Carro(String marca, String modelo, int ano, int potencia, int cilindrada, Tipo_Combustivel tipoCombustivel, double consumo100) {
         this.marca = marca;
@@ -22,4 +23,80 @@ public class Carro {
         this.tipoCombustivel = tipoCombustivel;
         this.consumo100 = consumo100;
     }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void ligar(){
+        int idade = 2025 - this.ano;
+
+        if(idade > 30){
+            if(this.tipoCombustivel.equals(Tipo_Combustivel.DISEL)){
+                System.out.println("Deita um pouco de fumo... Custa a pegar... O carro está ligado!");
+                System.out.println("Vrum-vrum-vrum");
+            }else {
+                System.out.println("Custa a pegar... O carro está ligado!");
+                System.out.println("Vrum-vrum-vrum");
+            }
+
+        }
+        if(idade <= 30){
+            if(this.potencia < 250){
+                System.out.println("O carro está ligado!");
+                System.out.println("Vrummmmmmmmmmmm");
+            }else {
+                System.out.println("O carro está ligado!");
+                System.out.println("VRUMMMMMMMM");
+            }
+
+        }
+
+
+    }
+
+public Carro corrida(Carro adversario){
+
+        if(this.potencia > adversario.potencia){
+            return this;
+        } else if (this.potencia < adversario.potencia) {
+            return adversario;
+
+        } else if (this.potencia == adversario.potencia) { //Bloco de empate de potencia
+            if(this.cilindrada > adversario.cilindrada){
+                return this;
+            }else if(this.cilindrada < adversario.cilindrada){
+                return adversario;
+
+            } else if (this.cilindrada == adversario.cilindrada) {//Bloco de empate de cilindrada
+                if(this.ano > adversario.ano){
+                    return this;
+                } else if (this.ano < adversario.ano) {
+                    return adversario;
+                }
+
+            }
+
+        }
+    return null;
+
 }
+
+
+public double consumoCombustivel(double distancia){
+        double valorConsumo = this.consumo100 * distancia;
+        return valorConsumo;
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
--- POO/FichaPratica_08/src/Ex_01/Main.java
+++ POO/FichaPratica_08/src/Ex_01/Main.java
@@ -7,7 +7,31 @@ public class Main {
 
 
 
-        Carro carro1 = new Carro("Seat","Ibiza", 2022,250,1800,Tipo_Combustivel.DISEL,8.5);
+        Carro carro1 = new Carro("Seat","Ibiza", 1990,250,1800,Tipo_Combustivel.DISEL,8.5);
+        Carro carro2 = new Carro("Ferrari", "F1", 2024,600,2500,Tipo_Combustivel.GASOLINA,15);
+
+        carro1.ligar();
+        carro2.ligar();
+
+
+        Carro vencedor1 = carro1.corrida(carro2);
+
+        System.out.println();
+
+        if(vencedor1 != null){
+            System.out.println("Vencedor é  " + vencedor1.getMarca());
+        }else {
+            System.out.println("Empate");
+        }
+
+        double consumo1 = carro1.consumoCombustivel(300);
+        double consumo2 = carro2.consumoCombustivel(300);
+
+        System.out.println();
+
+        System.out.println("O carro da marca " + carro1.getMarca() + " tem um consumo de " + consumo1);
+
+
 
     }
 }
